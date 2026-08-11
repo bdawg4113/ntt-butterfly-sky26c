cde8b08fc3dfedc828ae59cefbaac5f36212622b
bdawg4113 <brc016@ucsd.edu>
2026-08-11 06:32:20 -0700

    Finished cocotb check for te CT butterfly. Working on Twiddle ROM now.

---
+++ src/twiddle_rom.v
@@ -0,0 +1,9 @@
+module twiddle_rom(
+    input [6:0] addr,       // 7 bit wide addr
+    input [127:0] lut,      // 128 x 12 LUT
+                            // ROM[1] = 1729... ROM[127] 
+    output [11:0] zeta      // 12 bit wide ROM factor 
+);
+
+    
+endmodule
